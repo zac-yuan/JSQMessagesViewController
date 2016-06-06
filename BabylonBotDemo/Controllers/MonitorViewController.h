67fe3ba5f4ef310ee7a181019061b1ba7f016e30
Luka Bratos <luka.bratos@gmail.com>
2016-06-06 15:41:34 +0100

    Added Monitor VC to the Storyboard

---
+++ BabylonBotDemo/Controllers/MonitorViewController.h
@@ -0,0 +1,13 @@
+//
+//  MonitorViewController.h
+//  JSQMessages
+//
+//  Created by Luka Bratos on 06/06/2016.
+//  Copyright © 2016 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MonitorViewController : UIViewController
+
+@end
---
+++ BabylonBotDemo/Controllers/MonitorViewController.m
@@ -0,0 +1,37 @@
+//
+//  MonitorViewController.m
+//  JSQMessages
+//
+//  Created by Luka Bratos on 06/06/2016.
+//  Copyright © 2016 Hexed Bits. All rights reserved.
+//
+
+#import "MonitorViewController.h"
+
+@interface MonitorViewController ()
+
+@end
+
+@implementation MonitorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
